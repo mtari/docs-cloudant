cd1aed7c47a44864bbbb2136bdceddcc1ec49102
Tari Mihaly <mihaly.tari@ibm.com@taris-mbp.budapest-duna.hu.ibm.com>
2018-09-21 09:59:11 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # IBM Cloudant
 
-IBM Cloudant is a fully managed JSON document store for progressive web and mobile applications that maintain data access and availability at extreme scale.
+Cloudant is primarily delivered as a cloud-based service. Cloudant is a non-relational, distributed database service of the same name. Cloudant is based on the Apache-backed CouchDB project and the open source BigCouch project.
+
+Cloudant's service provides integrated data management, search, and analytics engine designed for web applications. Cloudant scales databases on the CouchDB framework and provides hosting, administrative tools, analytics and commercial support for CouchDB and BigCouch. Cloudant's distributed CouchDB service is used the same way as standalone CouchDB, with the added advantage of data being redundantly distributed over multiple machines.
+
+**Recommended:**
+You can read more about [Apache CouchDB](https://en.wikipedia.org/wiki/Apache_CouchDB)
+
+# HTTP API
+
+All requests to IBM Cloudant go over the web. This means any system that can speak to the web can speak to IBM Cloudant. All language-specific libraries for IBM Cloudant are really just wrappers that provide some convenience and linguistic niceties to help you work with a simple API. Many users choose to use raw HTTP libraries for working with IBM Cloudant.
+
+Specific details about how IBM Cloudant uses HTTP are provided in the [HTTP topic of the API Reference](https://console.bluemix.net/docs/services/Cloudant/api/http.html#http).
+
+# Cloudant Node.js Client
+We use [@cloudant/cloudant](https://www.npmjs.com/package/@cloudant/cloudant) in our applications. This library is the offical Cloudant library for Node.js.
+
--- test/cloudant.js
+++ test/cloudant.js
@@ -5,6 +5,7 @@ https://github.com/cloudant-labs/cloudant-nano
 https://console.bluemix.net/docs/services/Cloudant/basics/index.html#cloudant-basics
 */
 
+const Cloudant = require("@cloudant/cloudant");
 var should = require('should');
 
 const dbCredentials = {
@@ -15,10 +16,10 @@ let cloudant = null;
 
 before(() => {
     console.log("TEST before 1");
-    cloudant = require('@cloudant/cloudant')(dbCredentials.url);
+    cloudant = new Cloudant({ url: dbCredentials.url, plugins: 'promises' });
 });
 
-describe("Cloudant database functions", () => {
+describe.skip("Cloudant database functions", () => {
     it("replication function", (done) => {
         const dbName = 'animaldb';
         const dbReplicaName = 'animaldb-replica';
@@ -32,7 +33,7 @@ describe("Cloudant database functions", () => {
         });
     });
 
-    it.skip("replication.enable function", (done) => {
+    it("replication.enable function", (done) => {
         const url = "https://14d0e96f-539c-413a-b1b2-dfa47baaaf92-bluemix:24d869ab9780da53caa7cee957d62359868704f69497f65cce05d4b46d04f633@14d0e96f-539c-413a-b1b2-dfa47baaaf92-bluemix.cloudant.com";
         const dbName = `animaldb`;
         const dbURL = `${url}/${dbName}`;
@@ -53,6 +54,7 @@ describe("Cloudant database functions", () => {
         const dbURL = `${url}/${dbName}`;
         const dbReplicaName = `animaldb-replica-2`;
         const dbReplicaURL = `${url}/${dbReplicaName}`;
+
         cloudant.db.replication.enable(dbURL, dbReplicaURL, { create_target: true }, (err, body) => {
             if (err) {
                 throw err;
@@ -69,7 +71,7 @@ describe("Cloudant database functions", () => {
     });
 
     // TODO test replication.disable function
-    it.skip("replication.disable function", (done) => {
+    it("replication.disable function", (done) => {
         const url = "https://14d0e96f-539c-413a-b1b2-dfa47baaaf92-bluemix:24d869ab9780da53caa7cee957d62359868704f69497f65cce05d4b46d04f633@14d0e96f-539c-413a-b1b2-dfa47baaaf92-bluemix.cloudant.com";
         const dbName = `animaldb`;
         const dbURL = `${url}/${dbName}`;
@@ -112,130 +114,88 @@ describe("Cloudant database functions", () => {
 });
 
 describe("Cloudant document functions", () => {
-    before((done) => {
+    before(async () => {
         const dbName = "testdb";
-        cloudant.db.destroy(dbName, () => {
-            cloudant.db.create(dbName, (err, res) => {
-                if (err) {
-                    throw err;
-                }
-                db = cloudant.use(dbName);
-                done();
-            });
-        });
+        await cloudant.db.destroy(dbName);
+        await cloudant.db.create(dbName);
+        db = cloudant.use(dbName);
     });
 
-    it("Check if database exists", (done) => {
+    it("Check if database exists", async () => {
         const dbName = 'testdb';
-        cloudant.db.list((err, body, headers) => {
-            if (err) {
-                throw err;
-            }
-            body.should.containEql(dbName);
-            done();
-        });
+        const list = await cloudant.db.list();
+        list.should.containEql(dbName);
     });
 
     it("Should return an error when trying to access a document what doesn't exist", (done) => {
-        db.get("non-existing-doc", (err, data) => {
-            err.error.should.eql("not_found");
-            done();
-        });
+        db.get("non-existing-doc")
+            .catch((err) => {
+                err.error.should.eql("not_found");
+                done();
+            });
     });
 
-    it("Can insert document into a database without id", (done) => {
+    it("Can insert document into a database without id", async () => {
         const inputDoc = {
             title: "title 1",
             description: "test 1"
         };
 
-        db.insert(inputDoc, (err, body, header) => {
-            if (err) {
-                throw err;
-            }
-
-            db.get(body.id, (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                data.should.have.properties(inputDoc);
-                done();
-            });
-        });
+        const inputResult = await db.insert(inputDoc);
+        const getResult = await db.get(inputResult.id)
+        getResult.should.have.properties(inputDoc);
     });
 
-    it("Can insert document into a database using id (as a argument)", (done) => {
+    it("Can insert document into a database using id (as a argument)", async () => {
         const inputDoc = {
             title: "title 2",
             description: "test 2"
         };
         const id = "Test2ID";
 
-        db.insert(inputDoc, id, (err, body, header) => {
-            if (err) {
-                throw err;
-            }
-
-            db.get(id, (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                data.should.have.properties({ _id: id });
-                data.should.have.properties(inputDoc);
-                done();
-            });
-        });
+        const insertResult = await db.insert(inputDoc, id);
+        const getResult = await db.get(insertResult.id);
+        getResult.should.have.properties({ _id: id });
+        getResult.should.have.properties(inputDoc);
     });
 
-    it("Can insert document into a database using id (as part of the document)", (done) => {
+    it("Can insert document into a database using id (as part of the document)", async () => {
         const inputDoc = {
-            title: "title 2",
-            description: "test 2",
+            title: "title 3",
+            description: "test 3",
             _id: "Test3ID"
         };
 
-        db.insert(inputDoc, (err, body, header) => {
-            if (err) {
-                throw err;
-            }
-
-            db.get(inputDoc._id, (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                data.should.have.properties(inputDoc);
-                done();
-            });
-        });
+        await db.insert(inputDoc);
+        const getResult = await db.get(inputDoc._id);
+        getResult.should.have.properties(inputDoc);
     });
 
-    it("Can modify document", (done) => {
-        const inputID = "Test3ID"
+    it("Can modify document", async () => {
+        const inputDoc = {
+            title: "title 4",
+            description: "test 4"
+        };
+        const updatedTitle = "title 4 updated";
+        const updatedDescription = "test 4 updated";
 
-        db.get(inputID, (err, data) => {
-            if (err) {
-                throw err;
-            }
-            console.log(JSON.stringify(data));
-            const inputDoc = { ...data };
-            inputDoc.title = "title 2 updated";
-            inputDoc.description = "test 2 updated";
-            db.insert(inputDoc, (error, body, header) => {
-                if (error) {
-                    throw error;
-                }
-                done();
-            })
-        });
+        const insertResult = await db.insert(inputDoc);
+        const getResult = await db.get(insertResult.id);
+
+        const newInputDoc = {
+            ...getResult,
+            title: updatedTitle,
+            description: updatedDescription
+        };
+
+        const insertResult2 = await db.insert(newInputDoc);
+        const getResult2 = await db.get(insertResult2.id);
+        getResult2.title.should.eql(updatedTitle);
+        getResult2.description.should.eql(updatedDescription);
     });
 
-    it("can insert bulk data", (done) => {
+    it("can insert bulk data", async () => {
         const testData = [
-            {
-                title: "title 4",
-                description: "test 4",
-                _id: "Test4ID"
-            },
             {
                 title: "title 5",
                 description: "test 5",
@@ -245,77 +205,74 @@ describe("Cloudant document functions", () => {
                 title: "title 6",
                 description: "test 6",
                 _id: "Test6ID"
+            },
+            {
+                title: "title 7",
+                description: "test 7",
+                _id: "Test7ID"
             }
         ];
 
-        db.bulk({ docs: testData }, function (er) {
-            if (er) {
-                throw er;
-            }
-            done();
+        const result = await db.bulk({ docs: testData });
+        result.forEach(element => {
+            element.ok.should.eql(true);
         });
-
     });
 
-    it("can list data", (done) => {
-        db.list(function (err, body) {
-            if (err) {
-                throw err;
-            }
+    it("can list data", async () => {
+        let list = await db.list();
+        const originalLength = list.rows.length;
 
-            body.rows.forEach(function (doc) {
-                console.log(doc);
-            });
-            done();
-        });
+        const testData = [
+            {
+                title: "title 8",
+                description: "test 8",
+                _id: "Test8ID"
+            },
+            {
+                title: "title 9",
+                description: "test 9",
+                _id: "Test9ID"
+            }
+        ];
+
+        await db.bulk({ docs: testData });
+
+        list = await db.list();
+        const newLength = list.rows.length;
+
+        newLength.should.eql(originalLength + testData.length);
     })
 
-    it("Can copy document", (done) => {
+    it("Can copy document", async () => {
         const inputID = "Test2ID";
         const outputID = "Test2IDcopy";
 
-        db.copy(inputID, outputID, (err, body, header) => {
-            if (err) {
-                throw err;
-            }
-
-            console.log(body);
-            console.log(header);
-            done();
-        });
+        const result = await db.copy(inputID, outputID, { overwrite: false });
+        result.ok.should.eql(true);
     });
 
-    it("Returns registered indexes", (done) => {
-        db.index(function (er, result) {
-            if (er) {
-                throw er;
-            }
-
-            console.log('The database has %d indexes', result.indexes.length);
-            for (var i = 0; i < result.indexes.length; i++) {
-                console.log(JSON.stringify(result.indexes[i]));
-            }
-
-            result.should.have.a.property('indexes').which.is.an.Array;
-            done();
-        });
-    });
-
-    // TODO query data from testdb
-    it("Search should work", (done) => {
-        done();
+    it("Returns registered indexes", async () => {
+        const result = await db.index();
+        result.should.have.a.property('indexes').which.is.an.Array;
     });
 });
 
 describe.only("Query", () => {
-    before((done) => {
-        // TODO could remove the saved db, and then replicate the original
-        const dbName = "movies-demo";
-        db = cloudant.use(dbName);
-        done();
+    before(async () => {
+        // // TODO could remove the saved db, and then replicate the original
+        // // https://examples.cloudant.com/movies-demo/
+        // const dbName = "movies-demo";
+        // db = cloudant.use(dbName);
+        // done();
+
+        const dbURL = `https://examples.cloudant.com/movies-demo`;
+        const dbReplicaURL = `https://14d0e96f-539c-413a-b1b2-dfa47baaaf92-bluemix:24d869ab9780da53caa7cee957d62359868704f69497f65cce05d4b46d04f633@14d0e96f-539c-413a-b1b2-dfa47baaaf92-bluemix.cloudant.com/movies-demo`;
+        const result = await cloudant.db.replicate(dbURL, dbReplicaURL, { create_target: true });
+        console.log(result);
     });
 
-    it("Can query documents using number (greater than), but can't sort by 'Movie_year' (unless adding 'Movie_year' as a new index).", (done) => {
+    it.skip("Can query documents using number (greater than), but can't sort by 'Movie_year' (unless adding 'Movie_year' as a new index).", (done) => {
         // I only need to add this index, because I want to sort by 'Movie_year'
         const year = { name: "Movie_year", type: "json", index: { fields: ["Movie_year"] } };
         db.index(year, (error, response) => {
@@ -342,7 +299,7 @@ describe.only("Query", () => {
         });
     });
 
-    it("Selector basics", () => {
+    it.skip("Selector basics", () => {
         const query = {
             "selector": {
                 "Person_name": "Adam Sandler",
@@ -357,7 +314,7 @@ describe.only("Query", () => {
         });
     });
 
-    it("$and, $or and $eq operators", () => {
+    it.skip("$and, $or and $eq operators", () => {
         const query = {
             "selector": {
                 "$and": [
@@ -383,7 +340,7 @@ describe.only("Query", () => {
         });
     });
 
-    it("$and, $or and $eq operators 2", () => {
+    it.skip("$and, $or and $eq operators 2", () => {
         const query = {
             "selector": {
                 "Movie_year": 2006,
@@ -402,7 +359,7 @@ describe.only("Query", () => {
         });
     });
 
-    it("$and, $nor and $eq operator", () => {
+    it.skip("$and, $nor and $eq operator", () => {
         const query = {
             "selector": {
                 "$and": [
@@ -436,7 +393,7 @@ describe.only("Query", () => {
         });
     });
 
-    it("$and, $not and $eq operator", () => {
+    it.skip("$and, $not and $eq operator", () => {
         const query = {
             "selector": {
                 "$and": [
@@ -475,103 +432,52 @@ describe("Cloudant views and design functions", () => {
         db = cloudant.use(dbName);
     });
 
-    it("query view", (done) => {
-        db.view('views101', 'diet', {
+    it("Can query views for a single key.", async () => {
+        const result = await db.view('views101', 'diet', {
             'key': 'carnivore',
             'include_docs': true
-        }, function (err, body) {
-            console.log(err);
-            console.log(body);
-            if (!err) {
-                body.rows.forEach(function (doc) {
-                    console.log(JSON.stringify(doc));
-                });
-                done();
-            }
         });
+        result.rows.length.should.eql(2);
     });
 
-    it("query views with multiple keys", (done) => {
-        db.view('views101', 'diet', {
+    it("Can query views for multiple keys.", async () => {
+        const result = await db.view('views101', 'diet', {
             'keys': ['carnivore', 'herbivore']
-        }, function (err, body) {
-            if (err) {
-                throw err;
-            }
-            body.rows.forEach(function (doc) {
-                console.log(JSON.stringify(doc));
-            });
-            done();
         });
+        result.rows.length.should.eql(6);
     });
 
-    it("query views without params", (done) => {
-        db.view('views101', 'diet', function (err, body) {
-            if (err) {
-                throw err;
-            }
-            body.rows.forEach(function (doc) {
-                console.log(JSON.stringify(doc));
-            });
-            done();
-        });
-    });
-
-    // how can I make it work??? what is a format_doc and the doc_id???
-    it.skip("show should work", () => {
-        db.show('views101', 'format_doc', '3621898430', function (err, doc) {
-            if (err) {
-                throw err;
-            }
-            console.log(doc);
-            done();
-        });
+    it("Can query views without params.", async () => {
+        const result = await db.view('views101', 'diet');
+        result.rows.length.should.eql(10);
     });
 });
 
 describe("Cloudant search", () => {
     before(() => {
+        // TODO replicate DB
         const dbName = "animaldb";
         db = cloudant.use(dbName);
     });
 
-    it("search also works on views", (done) => {
-        db.search('views101', 'animals', { q: "zebra" }, function (err, body) {
-            if (err) {
-                throw err;
-            }
-            body.rows.forEach(function (doc) {
-                console.log(JSON.stringify(doc));
-            });
-            done();
+    it("Search should work on views using $text search", async () => {
+        const result = await db.search('views101', 'animals', { q: "zebra" });
+        result.total_rows.should.eql(1);
+    });
+
+    it("Search should work using a JSON search", async () => {
+        const result = await db.search('views101', 'animals', { q: "class: bird" });
+        result.total_rows.should.eql(2);
+    });
+
+    it("Can find animals starting with l.", async () => {
+        const result = await db.search('views101', 'animals', { q: "l*" });
+        result.rows.forEach(function (doc) {
+            console.log(JSON.stringify(doc));
         });
     });
 
-    it("search 1", (done) => {
-        db.search('views101', 'animals', { q: "class: bird" }, function (err, body) {
-            if (err) {
-                throw err;
-            }
-            body.rows.forEach(function (doc) {
-                console.log(JSON.stringify(doc));
-            });
-            done();
-        });
-    });
-
-    it("search 2", (done) => {
-        db.search('views101', 'animals', { q: "l*" }, function (err, body) {
-            if (err) {
-                throw err;
-            }
-            body.rows.forEach(function (doc) {
-                console.log(JSON.stringify(doc));
-            });
-            done();
-        });
-    });
-
-    it("search 3", (done) => {
+    it.skip("search 3", (done) => {
         db.search('views101', 'animals', { q: "class:bird AND diet:carnivore" }, function (err, body) {
             if (err) {
                 throw err;
@@ -583,7 +489,7 @@ describe("Cloudant search", () => {
         });
     });
 
-    it("search 4", (done) => {
+    it.skip("search 4", (done) => {
         db.search('views101', 'animals', { q: "l* AND diet:herbivore" }, function (err, body) {
             if (err) {
                 throw err;
@@ -595,7 +501,7 @@ describe("Cloudant search", () => {
         });
     });
 
-    it("search 5", (done) => {
+    it.skip("search 5", (done) => {
         db.search('views101', 'animals', { q: "min_length:[1 TO 3] AND diet:herbivore" }, function (err, body) {
             if (err) {
                 throw err;
@@ -607,7 +513,7 @@ describe("Cloudant search", () => {
         });
     });
 
-    it("search 6", (done) => {
+    it.skip("search 6", (done) => {
         db.search('views101', 'animals', { q: "diet:herbivore AND min_length:[-Infinity TO 2]" }, function (err, body) {
             if (err) {
                 throw err;
@@ -619,7 +525,7 @@ describe("Cloudant search", () => {
         });
     });
 
-    it("search 7", (done) => {
+    it.skip("search 7", (done) => {
         db.search('views101', 'animals', { q: "class:mammal AND min_length:[1.5 TO Infinity]" }, function (err, body) {
             if (err) {
                 throw err;
@@ -631,7 +537,7 @@ describe("Cloudant search", () => {
         });
     });
 
-    it("search 8", (done) => {
+    it.skip("search 8", (done) => {
         db.search('views101', 'animals', { q: "diet:(herbivore OR omnivore) AND class:mammal" }, function (err, body) {
             if (err) {
                 throw err;
